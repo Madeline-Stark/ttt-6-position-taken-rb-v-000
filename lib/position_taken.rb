cbc439f402d3360a5a714957e8a9050eba975b38
Madeline Stark <madeline.stark1@gmail.com>
2018-02-13 21:55:21 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,7 +1,8 @@
 # code your #position_taken? method here!
 def position_taken?(board, index)
+  board[index] = index_number
 if
-index == " " || "" || nil
+index_number == " " || "" || nil
  false
 else
   true
