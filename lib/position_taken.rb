3830e16a77aa8c7c86eee26190135190f00665ae
Madeline Stark <madeline.stark1@gmail.com>
2018-02-13 22:07:07 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,8 +1,14 @@
 # code your #position_taken? method here!
 def position_taken?(board, index)
 if
-board[index] == " " || "" || "nil"
+board[index] == " " 
  false
+elsif 
+  board[index] == ""
+  false
+elsif 
+  board[index] == "nil"
+  false
 else
   true
 end
